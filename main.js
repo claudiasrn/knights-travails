2c5d0fad77d669cdc5f0383589702e973d9b32fe
claudiasrn <perezserranoclaudia@gmail.com>
2026-07-26 05:11:49 +0200

    Build method to find shortest path using BFS

---
+++ main.js
@@ -0,0 +1,70 @@
+function knightMoves(start, end) {
+	let queue = [[start]];
+	let visited = new Set();
+	visited.add(start.toString());
+
+	while (queue.length > 0) {
+		let path = queue.shift();
+		let current = path[path.length - 1];
+
+		if (current[0] === end[0] && current[1] === end[1]) {
+			return path;
+		}
+
+		let moves = getPossibleMoves(current);
+
+		for (let move of moves) {
+			if (!visited.has(move.toString())) {
+				visited.add(move.toString());
+				let newPath = [...path, move];
+				queue.push(newPath);
+			}
+		}
+	}
+
+	function getPossibleMoves(square) {
+		let possibleMoves = [];
+
+		if (square[0] - 1 >= 0) {
+			if (square[1] - 2 >= 0) {
+				possibleMoves.push([square[0] - 1, square[1] - 2]);
+			}
+
+			if (square[1] + 2 <= 7) {
+				possibleMoves.push([square[0] - 1, square[1] + 2]);
+			}
+
+			if (square[0] - 2 >= 0) {
+				if (square[1] - 1 >= 0) {
+					possibleMoves.push([square[0] - 2, square[1] - 1]);
+				}
+
+				if (square[1] + 1 <= 7) {
+					possibleMoves.push([square[0] - 2, square[1] + 1]);
+				}
+			}
+		}
+
+		if (square[0] + 1 <= 7) {
+			if (square[1] - 2 >= 0) {
+				possibleMoves.push([square[0] + 1, square[1] - 2]);
+			}
+
+			if (square[1] + 2 <= 7) {
+				possibleMoves.push([square[0] + 1, square[1] + 2]);
+			}
+
+			if (square[0] + 2 <= 7) {
+				if (square[1] - 1 >= 0) {
+					possibleMoves.push([square[0] + 2, square[1] - 1]);
+				}
+
+				if (square[1] + 1 <= 7) {
+					possibleMoves.push([square[0] + 2, square[1] + 1]);
+				}
+			}
+		}
+
+		return possibleMoves;
+	}
+}
